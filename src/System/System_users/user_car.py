9a91a4a2b4049eddea756fef2f285088a8682190
Paulo Sérgio <paulosergiosrs08@gmail.com>
2024-08-14 09:32:50 -0300

    Add passwords
    Foi adcionado a funcionalidade de verificador de senhas

--- src/System/System_users/user_car.py
+++ src/System/System_users/user_car.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from datetime import datetime, date
 from uuid import uuid5
+import re
 
 class Users():
     def __init__(self):
@@ -14,7 +15,41 @@ class Users():
             'Senha': 'string'
         })
         
-        
+
+
+    def verificar_senha(self,senha):
+        """
+        Verifica a qualidade da senha com base em vários critérios:
+        - Comprimento mínimo de 8 caracteres
+        - Pelo menos uma letra maiúscula
+        - Pelo menos uma letra minúscula
+        - Pelo menos um número
+        - Pelo menos um caractere especial
+        """
+        criterios = { # dicionário com os valores da verificação de criterios
+            "comprimento_minimo": len(senha) >= 8,
+            "letra_maiuscula": re.search(r'[A-Z]', senha) is not None,
+            "letra_minuscula": re.search(r'[a-z]', senha) is not None,
+            "numero": re.search(r'\d', senha) is not None,
+            "caractere_especial": re.search(r'[^a-zA-Z0-9]', senha) is not None
+        }
+
+        todos_criterios_satisfeitos = all(criterios.values()) # capta os booleanos gerados
+
+        criterios, valido = criterios, todos_criterios_satisfeitos
+
+        if valido:
+            print("Senha válida!")
+            return True
+        else:
+            print("Senha inválida. Critérios não atendidos:")
+            for criterio, atendido in criterios.items():
+                if not atendido:
+                    print(f"- {criterio.replace('_', ' ').capitalize()}")
+                    return False
+                
+
+
     def Cadastro_User(self):
         codigo_user = f'USR{self._DataUsers.shape[0]+1:05d}'
         name = input('Informe nome do usuário')
@@ -34,5 +69,12 @@ class Users():
 
 if __name__ == '__main__':
     test = Users()
-    test.Cadastro_User()
-    test.listar()
+    # test.Cadastro_User()
+    # test.listar()
+
+    
+
+
+    # Testando a função
+    senhac = input("Digite a senha para verificar: ")
+    test.verificar_senha(senha=senhac)
