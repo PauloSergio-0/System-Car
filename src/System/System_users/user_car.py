43c13bd2186ac5a957f557e9bf0e0effb0a8c6cb
Paulo Sérgio <paulosergiosrs08@gmail.com>
2024-08-14 16:02:28 -0300

    Sistema de cadastro de user parcialmente integrado

--- src/System/System_users/user_car.py
+++ src/System/System_users/user_car.py
@@ -16,8 +16,8 @@ class Users():
         })
         
 
+    def verificar_senha(self): # ADD module uuid() hash
 
-    def verificar_senha(self,senha):
         """
         Verifica a qualidade da senha com base em vários critérios:
         - Comprimento mínimo de 8 caracteres
@@ -26,36 +26,50 @@ class Users():
         - Pelo menos um número
         - Pelo menos um caractere especial
         """
+        senha = input('Informe a senha: ')
+
         criterios = { # dicionário com os valores da verificação de criterios
             "comprimento_minimo": len(senha) >= 8,
             "letra_maiuscula": re.search(r'[A-Z]', senha) is not None,
             "letra_minuscula": re.search(r'[a-z]', senha) is not None,
             "numero": re.search(r'\d', senha) is not None,
-            "caractere_especial": re.search(r'[^a-zA-Z0-9]', senha) is not None
+            "caractere_especial": re.search(r'[^a-zA-Z0-9]', senha) is not None,
+            "Tem_espaços": re.search(r'\s', senha) is None
         }
-
         todos_criterios_satisfeitos = all(criterios.values()) # capta os booleanos gerados
 
         criterios, valido = criterios, todos_criterios_satisfeitos
 
         if valido:
             print("Senha válida!")
-            return True
+            return senha
         else:
             print("Senha inválida. Critérios não atendidos:")
             for criterio, atendido in criterios.items():
                 if not atendido:
                     print(f"- {criterio.replace('_', ' ').capitalize()}")
-                    return False
-                
+                    return self.verificar_senha()
+
+
+    def Editar_nome(self):
+        Codigo_seach = input('Informe o codigo: ')
+        if Codigo_seach in self._DataUsers['Codigo'].values:
+            index = self._DataUsers[self._DataUsers['Codigo'] == Codigo_seach].index[0]
+            new_name = input('Informe Novo nome:')
+            self._DataUsers.at[index,'Nome'] = new_name
+
+            print('Nome Atualizado!!')
+        else:
+            print('Código não encontrado')
+            return self.Editar_nome()
 
 
     def Cadastro_User(self):
         codigo_user = f'USR{self._DataUsers.shape[0]+1:05d}'
-        name = input('Informe nome do usuário')
+        name = input('Informe nome do usuário: ')
         idade = int(input("Informe a idade: "))
-        sexo = input('Informe Sexo')
-        senha = input('Informe a senha: ')
+        sexo = input('Informe Sexo: ')
+        senha = self.verificar_senha() 
         self._DataUsers.loc[self._DataUsers.shape[0]] =[
             codigo_user,
             name,
@@ -63,18 +77,14 @@ class Users():
             sexo,
             senha
         ]
-            
+
+
     def listar(self):
         print(self._DataUsers)
 
 if __name__ == '__main__':
     test = Users()
-    # test.Cadastro_User()
-    # test.listar()
-
-    
-
-
-    # Testando a função
-    senhac = input("Digite a senha para verificar: ")
-    test.verificar_senha(senha=senhac)
+    test.Cadastro_User()
+    test.listar()
+    test.Editar_nome()
+    test.listar()
