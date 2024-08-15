6335311febafebca022d92c1b53e1175d3e3ecac
Paulo Sérgio dev ofc <125888492+PauloSergio-0@users.noreply.github.com>
2024-08-15 22:02:59 +0000

    Inicializando o main

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import System_Cadastro.Cadastro_car as Cadastro_car
+import System_Vendas.Venda_car as Venda_car
+import System_users.user_car as user_car
+
+
+
+if __name__ == '__main__':
+    user = user
